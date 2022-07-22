7b5771b372d75ac0612f80529226c72649694813
Kevyn <kevyn.santos@mindsatwork.com.br>
2022-07-22 03:13:27 -0300

    Adicionando rotas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
-import Evento from './components/Evento';
-import Form from './components/Form';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { Routes  } from 'react-router-dom';
+import Home from './pages/Home'
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
 
 function App() {
 
   return (
-    <div className="App">
-        <h1>Testando Eventos</h1>
-        <Evento/>
-        <Form/>
-    </div>
+    <Router>
+      <ul>
+          <li>
+              <Link to="/">Home</Link>
+          </li>
+          <li>
+              <Link to="/empresa">Empresa</Link>
+          </li>
+          <li>
+              <Link to="/contato">Contato</Link>
+          </li>
+      </ul>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/contato" element={<Contato/>}/>
+          <Route exact path="/empresa" element={<Empresa/>}/>
+        </Routes>
+    </Router>
   )
 }
 
