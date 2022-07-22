61d3ad609e7cdcd7baa5cec136d13877a18ca0f1
Kevyn <kevyn.santos@mindsatwork.com.br>
2022-07-22 02:51:17 -0300

    Inclusão de estados

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,20 @@
+import {useState} from 'react'
+
 function Form(){
+
+    const [name,setName] = useState('Kevyn')
 
     function cadastro(e){
         e.preventDefault();
         console.log('Cadastro realizado');
+        console.log(name);
     }
 
     return(
         <div>
             <h1>Meu Cadastro</h1>
             <form onSubmit={cadastro}>
-                <input type="text" placeholder="Digite seu nome"/>
+                <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Digite seu nome"/>
                 <input type="submit" value="Enviar"/>
             </form>
         </div>
